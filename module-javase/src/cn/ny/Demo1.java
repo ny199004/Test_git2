778fa433875da1b9554efc6497115e655c179e45
聂野 <2691439293@qq.com>
2019-06-09 20:19:38 +0800

    add  Demo1

---
+++ module-javase/src/cn/ny/Demo1.java
@@ -0,0 +1,11 @@
+package cn.ny;
+
+/**
+ * @author nieye
+ * @create 2019-06-09 20:18
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("asdad");
+    }
+}
